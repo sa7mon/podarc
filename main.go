4b0a303db9da2074d48bc216ba6fdb0522d19386
Dan Salmon <dan.sa7mon@gmail.com>
2020-04-21 22:36:23 -0500

    Get Unmarshaling working

--- main.go
+++ main.go
@@ -1,11 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
-func main() {
-	fmt.Println("Hello, world.")
+type StitcherPodcast struct {
+	Feed 			StitcherFeed 	  `xml:"feed"`
+	ShowDescription string 	   		  `xml:"show_description,attr"`
+	Episodes 		[]StitcherEpisode `xml:"episodes>episode"`
 }
 
-func getPodcastFeed() {
+type StitcherEpisodes struct {
+	Episodes []StitcherEpisode
+}
+type StitcherFeed struct {
+	Name 		  string 		  `xml:"name"`
+	Description   string 		  `xml:"description"`
+	LatestEpisode StitcherEpisode `xml:"episode"`
+	Premium 	  bool 			  `xml:"premium,attr"`
+	EpisodeCount  int 			  `xml:"episodeCount,attr"`
+}
 
+type StitcherEpisode struct {
+	Id          string    `xml:"id,attr"`
+	Image       string    `xml:"episodeImage,attr"`
+	Published   time.Time `xml:"published,attr"`
+	Title       string    `xml:"title"`
+	Description string    `xml:"description"`
+	Url 		string 	  `xml:"url, attr"`
+}
+
+func main() {
+	officeLadies := getStitcherPodcastFeed("","")
+	log.Println(officeLadies.ShowDescription)
+
+	for _, element := range officeLadies.Episodes {
+		log.Println(element.Title)
+	}
+}
+
+func getStitcherPodcastFeed(feedId string, sess string) *StitcherPodcast {
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://app.stitcher.com/Service/GetFeedDetailsWithEpisodes.php?" +
+								"mode=webApp&fid=%s&max_epi=5000&sess=%s", feedId, sess), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("Bad status code while getting podcast - " + resp.Status)
+	}
+
+	podcast := &StitcherPodcast{}
+
+	xmlDecoder := xml.NewDecoder(resp.Body)
+	err = xmlDecoder.Decode(podcast)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return podcast
 }
