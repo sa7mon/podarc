812afa8fc263af0e87b32016d4375a6b49b8dfde
Dan Salmon <salmon@protonmail.ch>
2020-04-26 18:36:19 -0500

    Remove test file and move tagging stuff to archiver

--- internal/archiver/archiver.go
+++ internal/archiver/archiver.go
@@ -2,7 +2,6 @@ package archiver
 
 import (
 	"github.com/sa7mon/podarc/internal/id3-go"
-	v2 "github.com/sa7mon/podarc/internal/id3-go/v2"
 	"github.com/sa7mon/podarc/internal/interfaces"
 	"github.com/sa7mon/podarc/internal/utils"
 	"log"
@@ -49,31 +48,33 @@ func ArchivePodcast(podcast interfaces.Podcast, destDirectory string, overwriteE
 }
 
 func WriteID3TagsToFile(filePath string, episode interfaces.PodcastEpisode, podcast interfaces.Podcast) error {
-	file, err := id3.Open(filePath)
+	/*
+		Hacky workaround because the library doesn't support deleting ID3v1 tags.
+		We need to use ID3v2 because v1 has a 30-character limit on the title field (and likely others).
+		If the file has v1 tags, re-open forcing v2 tags which effectively erases all existing tags
+		that we don't set here.
+	*/
+
+	file, err := id3.Open(filePath, false)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	log.Println("Version: " + file.Version())
+	if file.Version()[0:1] == "1" {
+		// Re-open the file, forcing v2
+		log.Println("ID3v1 detected. Re-opening file and forcing ID3v2...")
+		file.Close()
+		file, err = id3.Open(filePath, true)
+		if err != nil {
+			log.Println(err)
+		}
+		defer file.Close()
+	}
 
 	file.SetArtist(podcast.GetTitle())
 	file.SetTitle(episode.GetTitle())
 	file.SetGenre("Podcast")
-
-	//log.Println("Detected v2 tag. Writing publisher...")
-	ft := v2.V23FrameTypeMap["TIT2"]
-	titleFrame := v2.NewTextFrame(ft, episode.GetTitle())
-	//allFrames = append(allFrames, textFrame)
-	file.AddFrames(titleFrame)
-
-	//if file.Tagger.Version()[0:1] == "2" {
-	//	log.Println("Detected v2 tag. Writing publisher...")
-	//	ft := v2.V23FrameTypeMap["TPUB"]
-	//	textFrame := v2.NewTextFrame(ft, podcast.GetPublisher())
-	//	//allFrames = append(allFrames, textFrame)
-	//	file.AddFrames(textFrame)
-	//}
 
 	// Set year recorded
 	// Save podcast publisher to one of the tags
