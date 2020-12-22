26dc461a9c34f975e83bdb587c2360bcfd67b008
Dan Salmon <dan.sa7mon@gmail.com>
2020-12-21 20:23:46 -0600

    Start getting new stitcher provider working

--- internal/providers/common.go
+++ internal/providers/common.go
@@ -11,12 +11,18 @@ import (
 func FetchPodcastFromUrl(feedUrl string, creds utils.Credentials) (interfaces.Podcast, error) {
 	stitcherR := regexp.MustCompile(`https://app\.stitcher\.com/browse/feed/(?P<feedId>\d+)`)
 	libsynR := regexp.MustCompile(`\S+\.libsynpro.com/rss`)
+	stitcherNewR := regexp.MustCompile(`https://www\.stitcher\.com/show/(?P<slug>[a-zA-Z0-9-]+)`)
+
 	libSynMatches := libsynR.MatchString(feedUrl)
 	stitcherMatches := stitcherR.FindStringSubmatch(feedUrl)
+	stitcherNewMatches := stitcherNewR.FindStringSubmatch(feedUrl)
 
 	if len(stitcherMatches) > 0 {
 		stitcherPod := GetStitcherPodcastFeed(stitcherMatches[1], creds.SessionToken)
 		return stitcherPod, nil
+	} else if len(stitcherNewMatches) > 0 {
+		stitcherNewPod := GetStitcherNewPodcastFeed(stitcherNewMatches[1])
+		return stitcherNewPod, nil
 	} else if libSynMatches {
 		libsynPod := GetLibsynProPodcastFeed(feedUrl)
 		return libsynPod, nil
