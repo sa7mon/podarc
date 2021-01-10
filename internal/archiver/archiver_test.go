9dc6e3e8dc32281bc45423a817b9fe85504ddb52
Dan Salmon <salmon@protonmail.ch>
2021-01-09 19:22:25 -0600

    Add test

--- internal/archiver/archiver_test.go
+++ internal/archiver/archiver_test.go
@@ -1,4 +1,19 @@
 package archiver
 
-import testing
+import (
+	"github.com/sa7mon/podarc/internal/providers"
+	"github.com/sa7mon/podarc/test"
+	"testing"
+)
 
+func TestGetFileNameFromEpisodeURL(t *testing.T) {
+	ep := providers.GenericEpisode{}
+	ep.Enclosure.URL = "https://my.site/file.mp3"
+
+	fileName := GetFileNameFromEpisodeURL(ep)
+
+	test.AssertString(t, "fileName", "file.mp3", fileName)
+
+	ep.Enclosure.URL = "https://my.site/file.mp3?asdf=1"
+	test.AssertString(t, "fileName2", "file.mp3", fileName)
+}
