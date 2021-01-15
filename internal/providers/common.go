5f39c15a65ff8f749d301e4d898ee34a2a0d2459
Dan Salmon <salmon@protonmail.ch>
2021-01-15 14:40:07 -0600

    Remove handling for old Stitcher feed

--- internal/providers/common.go
+++ internal/providers/common.go
@@ -7,19 +7,14 @@ import (
 )
 
 func FetchPodcastFromURL(feedURL string, creds utils.Credentials) (interfaces.Podcast, error) {
-	stitcherR := regexp.MustCompile(`https://app\.stitcher\.com/browse/feed/(?P<feedId>\d+)`)
 	libsynR := regexp.MustCompile(`\S+\.libsynpro.com/rss`)
-	stitcherNewR := regexp.MustCompile(`https://www\.stitcher\.com/show/(?P<slug>[a-zA-Z0-9-]+)`)
+	stitcherR := regexp.MustCompile(`https://www\.stitcher\.com/show/(?P<slug>[a-zA-Z0-9-]+)`)
 
 	libSynMatches := libsynR.MatchString(feedURL)
 	stitcherMatches := stitcherR.FindStringSubmatch(feedURL)
-	stitcherNewMatches := stitcherNewR.FindStringSubmatch(feedURL)
 
 	if len(stitcherMatches) > 0 {
-		stitcherPod := GetStitcherPodcastFeed(stitcherMatches[1], creds.SessionToken)
-		return stitcherPod, nil
-	} else if len(stitcherNewMatches) > 0 {
-		stitcherNewPod := GetStitcherNewPodcastFeed(stitcherNewMatches[1], creds.StitcherNewToken)
+		stitcherNewPod := GetStitcherPodcastFeed(stitcherMatches[1], creds.StitcherNewToken)
 		return stitcherNewPod, nil
 	} else if libSynMatches {
 		libsynPod := GetLibsynProPodcastFeed(feedURL)
