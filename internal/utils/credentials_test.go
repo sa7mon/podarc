4444c1dd94f795babf558b04eee59116ccd7aedf
Dan Salmon <salmon@protonmail.ch>
2021-01-15 11:01:23 -0600

    Fix stitcher credentials test

--- internal/utils/credentials_test.go
+++ internal/utils/credentials_test.go
@@ -1,8 +1,24 @@
 package utils
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"testing"
+	"time"
 )
+
+type StitcherTokenPayload struct {
+	Sub             string   `json:"sub"`
+	Aud             string   `json:"aud"`
+	CognitoGroups   []string `json:"cognito:groups"`
+	EmailVerified   bool     `json:"email_verified"`
+	AuthTime        int      `json:"auth_time"`
+	Iss             string   `json:"iss"`
+	CognitoUsername string   `json:"cognito:username"`
+	Exp             int      `json:"exp"`
+	Iat             int      `json:"iat"`
+	Email			string	 `json:"email"`
+}
 
 func TestReadCredentials(t *testing.T) {
 	testCreds := ReadCredentials("credentials_test.json")
@@ -30,14 +46,32 @@ func TestIsStitcherTokenValid(t *testing.T) {
 		t.Errorf("invalid JSON Base64 token was validated or reason was wrong: '%s'", invalidJSONBase64isValidReason)
 	}
 
-	jwtMissingEmail := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhc2RmYXNkZiIsImF1ZCI6ImFzZGZhc2RmIiwiY29nbml0bzpncm91cHMiOlsiUHJlbWl1bSJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXV0aF90aW1lIjoxNjA5ODE0NTcwLCJpc3MiOiJodHRwczovL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tL3VzLWVhc3QtMV9hc2RmIiwiY29nbml0bzp1c2VybmFtZSI6ImFzZGYxMjM0IiwiZXhwIjoxNjA5ODkyMTQ0LCJpYXQiOjE2MDk4ODg1NDUsImVtYWlsIjoiYXNkZkBlbWFpbC5jb20ifQ.Z41mfxmgR0CsdXs_UTqddKMSwNlBqINHmxOWfdT7Vng"
+	jwtMissingEmail := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhc2RmYXNkZiIsImF1ZCI6ImFzZGZhc2RmIiwiY29nbml0bzpncm91cHMiOlsiUHJlbWl1bSJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXV0aF90aW1lIjoxNjA5ODE0NTcwLCJpc3MiOiJodHRwczovL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tL3VzLWVhc3QtMV9hc2RmIiwiY29nbml0bzp1c2VybmFtZSI6ImFzZGYxMjM0IiwiZXhwIjoxNjA5ODkyMTQ0LCJpYXQiOjE2MDk4ODg1NDV9.e1ssVLtMv8ZO2KZ2KHdCTVQlNNUIbNXGDUzI2a2s138"
 	missingEmailIsValid, missingEmailIsValidReason := IsStitcherTokenValid(jwtMissingEmail)
 	if missingEmailIsValid || missingEmailIsValidReason != "JWT missing fields" {
 		t.Errorf("JWT with email missing was validated or reason was wrong: '%s'", missingEmailIsValidReason)
 	}
 
-	goodJWT := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhc2RmYXNkZiIsImF1ZCI6ImFzZGZhc2RmIiwiY29nbml0bzpncm91cHMiOlsiUHJlbWl1bSJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXV0aF90aW1lIjoxNjA5ODE0NTcwLCJpc3MiOiJodHRwczovL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tL3VzLWVhc3QtMV9hc2RmIiwiY29nbml0bzp1c2VybmFtZSI6ImFzZGYxMjM0IiwiZXhwIjoxNjA5ODkyMTQ0LCJpYXQiOjE2MDk4ODg1NDUsImVtYWlsIjoiYXNkZkBlbWFpbC5jb20ifQ.Z41mfxmgR0CsdXs_UTqddKMSwNlBqINHmxOWfdT7Vng"
-	goodJWTisValid, goodJWTisValidReason := IsStitcherTokenValid(goodJWT)
+	j, err := json.Marshal(StitcherTokenPayload{
+		Aud: "",
+		Sub: "",
+		CognitoGroups: []string{"Premium"},
+		EmailVerified: true,
+		AuthTime: 1609814570,
+		Iss: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_asdf",
+		CognitoUsername: "asdf1234",
+		Exp: int(time.Now().Unix() + 300),   // 5 minutes from now
+		Iat: 1609888545,
+		Email: "asdf@asdf.lol",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	goodToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." + base64.URLEncoding.EncodeToString(j) +
+				".Z41mfxmgR0CsdXs_UTqddKMSwNlBqINHmxOWfdT7Vng"
+
+	goodJWTisValid, goodJWTisValidReason := IsStitcherTokenValid(goodToken)
 	if !goodJWTisValid  {
 		t.Errorf("Valid token was not validated: '%s'", goodJWTisValidReason)
 	}
