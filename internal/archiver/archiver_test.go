956d01a19b93f00494222025f732c21a83455cd2
Dan Salmon <salmon@protonmail.ch>
2021-01-15 18:39:09 -0600

    Add tests

--- internal/archiver/archiver_test.go
+++ internal/archiver/archiver_test.go
@@ -26,5 +26,20 @@ func TestGetFileNameFromEpisodeURL(t *testing.T) {
 	if err == nil {
 		t.Error("Bad URL didn't return an error")
 	}
+}
 
+func TestGetEpisodeFileName(t *testing.T) {
+	//layout := "Mon, 02 Jan 2006 15:04:05 -0700" // Mon Jan 2 15:04:05 MST 2006
+	//jan02, err := time.Parse(layout, "Mon, 02 Jan 2006 15:04:05 -0700")
+	//if err != nil {
+	//	t.Error(err)
+	//}
+
+	genericEpisode := providers.GenericEpisode{Title: "My Cool Episode", PubDate: "Mon, 02 Jan 2006 15:04:05 -0700"}
+
+	name1 := GetEpisodeFileName("my_cool_episode.mp3", genericEpisode)
+	test.AssertString(t, "GetEpisodeFileName_good", "2006-01-02_my-cool-episode.mp3", name1)
+
+	name2 := GetEpisodeFileName("my_cool_episode.mp3?tracking_tag=asdf", genericEpisode)
+	test.AssertString(t, "GetEpisodeFileName_good", "2006-01-02_my-cool-episode.mp3", name2)
 }
