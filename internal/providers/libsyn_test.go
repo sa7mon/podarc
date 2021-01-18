b38edb542f75818eb444f830798d4acb5003fee2
Dan Salmon <salmon@protonmail.ch>
2021-01-18 08:49:30 -0600

    Add tests

--- internal/providers/libsyn_test.go
+++ internal/providers/libsyn_test.go
@@ -1,9 +1,11 @@
 package providers
 
 import (
+	"fmt"
 	"github.com/sa7mon/podarc/internal/utils"
 	"github.com/sa7mon/podarc/test"
 	"testing"
+	"time"
 )
 
 func TestLibsynUnmarshal(t *testing.T) {
@@ -20,6 +22,21 @@ func TestLibsynUnmarshal(t *testing.T) {
 	test.AssertString(t, "Episode URL", "http://traffic.libsyn.com/mates/MATES51_Peruvian_Snacks.mp3?dest-id=50920", firstEpisode.GetURL())
 	test.AssertString(t, "Episode Published Date", "Mon, 05 Mar 2012 08:00:00 +0000", firstEpisode.GetPublishedDate())
 	test.AssertString(t, "Episode Image URL", "http://static.libsyn.com/p/assets/8/d/b/d/8dbd7e032866e1a8/MATES_logo.jpg", firstEpisode.GetImageURL())
+
+	jan_1_layout := "Mon, 02 Jan 2006 15:04:05 -0700" // Mon Jan 2 15:04:05 MST 2006
+	jan_1, err := time.Parse(jan_1_layout, "Mon, 01 Jan 2001 01:01:01 -0000")
+	if err != nil {
+		t.Error("Couldn't get test date ready: " + err.Error())
+	}
+
+	firstEpPubDate, err := firstEpisode.GetParsedPublishedDate()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !firstEpPubDate.After(jan_1) {
+		t.Error("Latest episode's parsed published date was not after Jan 1 2001: " + firstEpPubDate.String())
+	}
 }
 
 func TestFetchPodcastFromUrl(t *testing.T) {
@@ -30,4 +47,21 @@ func TestFetchPodcastFromUrl(t *testing.T) {
 	}
 
 	test.AssertTypesAreEqual(t, p, &LibsynPodcast{})
+}
+
+func TestLibSynEpisode_ToString(t *testing.T) {
+	title := "My Cool Episode"
+	description := "It's very cool!"
+	link := "https://my.cool.podcast/episode1"
+	imageLink := "https://my.cool.podcast/episode1.jpeg"
+	publishedDate := "2020-01-05 10:35:42"
+
+	g := LibsynEpisode{Title: title, Description: description, Published: publishedDate}
+	g.Enclosure.Url = link
+	g.Image.ImageURL = imageLink
+
+	expected := fmt.Sprintf("Title: %s | Description: %s | Url: %s | PublishedDate: " +
+		"%s | ImageUrl: %s", title, description, link, publishedDate, imageLink)
+
+	test.AssertString(t, "LibsynEpisode_toString", expected, g.ToString())
 }
